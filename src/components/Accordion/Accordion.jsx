61afce482d0dadbad3deaac2006e0e9392cd4e23
AlexBabkf <bab_alt@hotmail.com>
2024-02-15 11:14:26 +0100

    added multiple selection option

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -3,20 +3,60 @@ import { accordionData } from "./accordionData";
 
 export default function Accordion() {
   const [selected, setSelected] = useState(null);
+  const [multipleSelection, setMultipleSelection] = useState(false);
+  const [multipleSelected, setMultipleSelected] = useState([]);
 
   function handleShowAnswer(id) {
     setSelected(selected === id ? null : id);
   }
 
+  function handleMultipleSelection() {
+    setMultipleSelection(!multipleSelection);
+    setSelected(null);
+    setMultipleSelected([]);
+  }
+
+  function handleMultipleShowAnswer(id) {
+    const selectedCopy = [...multipleSelected];
+    const currentIdIndex = selectedCopy.indexOf(id);
+
+    if (currentIdIndex === -1) {
+      selectedCopy.push(id);
+    } else {
+      selectedCopy.splice(currentIdIndex, 1);
+    }
+    setMultipleSelected(selectedCopy);
+  }
+
+  console.log(multipleSelected);
+
   return (
     <div>
-      {accordionData.map((item) => (
-        <div key={item.id}>
-          <h3>{item.question}</h3>
-          <span onClick={() => handleShowAnswer(item.id)}>+</span>
-          {selected === item.id ? <h3>{item.answer}</h3> : null}
-        </div>
-      ))}
+      <button onClick={handleMultipleSelection}>
+        {multipleSelection
+          ? "Disable Multiple Selection"
+          : "Enable Multiple Selection"}
+      </button>
+      <div>
+        {accordionData.map((item) => (
+          <div key={item.id}>
+            <h3>{item.question}</h3>
+            <span
+              onClick={
+                multipleSelection
+                  ? () => handleMultipleShowAnswer(item.id)
+                  : () => handleShowAnswer(item.id)
+              }
+            >
+              +
+            </span>
+            {selected === item.id ||
+            multipleSelected.indexOf(item.id) !== -1 ? (
+              <h3>{item.answer}</h3>
+            ) : null}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
