4138d8d0e0ad597453868d604aa762235fb0ced0
AlexBabkf <bab_alt@hotmail.com>
2024-02-15 12:58:39 +0100

    created random color generator

---
+++ src/components/Random-Color/RandomColor.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+
+export default function RandomColor() {
+  const [colorType, setColorType] = useState("HEX");
+  const [color, setColor] = useState("#000000");
+
+  function randomize(length) {
+    return Math.floor(Math.random() * length);
+  }
+
+  function handleGenerateHEX() {
+    const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+    let hexcolor = "#";
+    for (let i = 0; i < 6; i++) {
+      hexcolor += hex[randomize(hex.length)];
+    }
+    console.log(hexcolor);
+
+    setColor(hexcolor);
+  }
+
+  function handleGenerateRGB() {
+    const r = randomize(256);
+    const g = randomize(256);
+    const b = randomize(256);
+
+    console.log(r, g, b);
+
+    setColor(`rgb(${r},${g},${b})`);
+  }
+
+  useEffect(() => {
+    setColor(colorType === "RGB" ? "rgb(256,256,256)" : "#FFFFFF");
+  }, [colorType]);
+
+  console.log(color, colorType);
+
+  return (
+    <div style={{ background: color }}>
+      <div>
+        <button onClick={() => setColorType("HEX")}>Create HEX color</button>
+        <button onClick={() => setColorType("RGB")}>Create RGB color</button>
+        <button
+          onClick={colorType === "HEX" ? handleGenerateHEX : handleGenerateRGB}
+        >
+          Generate Random Color
+        </button>
+      </div>
+      <div>
+        <h2>
+          {colorType} {color}
+        </h2>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import Accordion from "@/components/Accordion/Accordion";
+import RandomColor from "@/components/Random-Color/RandomColor";
 
 export default function Home() {
   return (
     <>
       <Accordion />
+      <RandomColor />
     </>
   );
 }
